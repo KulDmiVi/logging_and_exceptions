4d4e17be2829c0db2f4b113ffccaa98dbfc4b3ff
KulDmiVi <kulDmiVi@gmail.com>
2024-06-24 10:44:45 +0300

    Внесены изменения в логику обработки исключений

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,11 +4,13 @@ import math
 def input_data():
     try:
         a = float(input("a = "))
+        if a == 0.0:
+            raise ValueError
         b = float(input("b = "))
         c = float(input("c = "))
         return a, b, c
     except ValueError:
-        print("Ошибка ввода! Коэффициенты a, b, c - числа")
+        print("Ошибка ввода! Коэффициенты a, b, c - числа и a равно 0")
         return None
 
 
@@ -19,20 +21,12 @@ def get_discr(a, b, c):
 
 def calc_roots_equation(a, b, discr):
     if discr > 0:
-        try:
-            x1 = round((-b + math.sqrt(discr)) / (2 * a), 2)
-            x2 = round((-b - math.sqrt(discr)) / (2 * a), 2)
-            return x1, x2
-        except ZeroDivisionError:
-            return "Коэффициент 'а' должен быть равен = 0 "
-
+        x1 = round((-b + math.sqrt(discr)) / (2 * a), 2)
+        x2 = round((-b - math.sqrt(discr)) / (2 * a), 2)
+        return x1, x2
     elif discr == 0:
-        try:
-            x = round(-b / (2 * a), 2)
-            return x
-        except ZeroDivisionError:
-            return "Коэффициент 'а' должен быть равен = 0 "
-
+        x = round(-b / (2 * a), 2)
+        return x
     else:
         return 'Уравнение не имеет действительных корней'
 
