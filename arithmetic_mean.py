4da98e2d7f6cdb3cface103c386ea8a0f13a2c21
KulDmiVi <kulDmiVi@gmail.com>
2024-06-25 09:24:40 +0300

    Добавлено логирование

--- arithmetic_mean.py
+++ arithmetic_mean.py
@@ -1,14 +1,21 @@
+import logger
+
+logger = logger.get_logger(__name__, 'arithmetic_mean.log')
+
+
 def get_input_data():
     string_list = input('Введите числа через запятую: ').split(',')
     try:
         numbers = tuple(map(float, string_list))
+        logger.info(f"Введена последовательность {numbers}")
         return numbers
     except ValueError:
-        print("Ошибка ввода данных")
+        logger.error("Ошибка ввода данных")
 
 
 def arithmetic_mean(numbers):
     average = sum(numbers) / len(numbers)
+    logger.info(f"Вычислено среднее арифметическое")
     return average
 
 
@@ -19,8 +26,10 @@ def main():
 
 
 if __name__ == '__main__':
+    logger.info("Старт программы")
     result = None
     while not result:
         result = main()
     else:
         print(f"Cреднее арифметическое чисел: {result}")
+        logger.info("Работа завершена")
